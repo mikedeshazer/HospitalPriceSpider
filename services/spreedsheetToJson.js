c2cb993465a7984d9a6eb8d328956fca4ecc49b8
Nicholas Babu <nijoba33@gmail.com>
2019-09-13 18:58:37 +0300

    removed comments

--- services/spreedsheetToJson.js
+++ services/spreedsheetToJson.js
@@ -21,47 +21,7 @@ async function convertXlsxToJson(filePath) {
     }
 
 }
-
-/*
-const spreadsheetKey = "17v-jZUsnU5Hl6l4GpOtd-cQ8maOMY8OMC5TYzmuUZPQ";
-//const credentials = require("./client_id.json");
-
-extractSheets(
-    {
-        spreadsheetKey
-        // credentials
-    },
-    (err, data) => {
-        if (err) {
-            console.log("ERROR:", err);
-        }
-        console.log('Data', JSON.stringify({ err, data }, null, 2));
-    }
-);*/
-
-/*
-
-// optional custom format cell function
-const formatCell = (sheetTitle, columnTitle, value) => value.toUpperCase();
-
-extractSheets(
-    {
-        // your google spreadhsheet key
-        //https://docs.google.com/spreadsheets/d/17v-jZUsnU5Hl6l4GpOtd-cQ8maOMY8OMC5TYzmuUZPQ/edit?usp=sharing
-        spreadsheetKey: "17v-jZUsnU5Hl6l4GpOtd-cQ8maOMY8OMC5TYzmuUZPQ",
-        // your google oauth2 credentials (optional for world-readable spreadsheets)
-        //credentials: require("./google-generated-creds.json"),
-        // optional: names of the sheets you want to extract
-        //sheetsToExtract: ["Customers", "Invoices"],
-        // optional: custom function to parse the cells
-        formatCell: formatCell
-    },
-    function(err, data) {
-        console.log("Customers: ", data);
-        console.log("Invoices: ", data);
-    }
-);*/
-
+ 
 module.exports = {
     convertXlsxToJson,
 }
